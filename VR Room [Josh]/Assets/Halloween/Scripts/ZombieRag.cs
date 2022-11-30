e9d75060247ea56562293ec2ecf08aac48fc80ed
joshbxtr <A00286199@student.ait.ie>
2022-11-30 10:53:00 +0000

    /

--- VR Room [Josh]/Assets/Halloween/Scripts/ZombieRag.cs
+++ VR Room [Josh]/Assets/Halloween/Scripts/ZombieRag.cs
@@ -1,25 +1,77 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class ZombieRag : MonoBehaviour
 {
+    public BoxCollider mainCollider;
+    public GameObject rig;
+    public Rigidbody rb;
+    public NavMeshAgent nav;
+    public EnemyFollow follow;
+
     // Start is called before the first frame update
     void Start()
     {
-        Rigidbody[] bodies = GetComponentsInChildren<Rigidbody>();
-
-        foreach (var b in bodies)
-            b.isKinematic = true;
+        GetRagdollBits();
+        ragdollOff();
     }
 
-    public void Kill()
+    private void OnCollisionEnter(Collision collision)
     {
-        Rigidbody[] bodies = GetComponentsInChildren<Rigidbody>();
-
-        foreach (var b in bodies)
-            b.isKinematic = false;
+        if (collision.gameObject.tag == "Weapon")
+        {
+            ragdollOn();
+            Debug.Log("Ragdoll On");
+       
+      
+        }
     }
+
+    public Collider[] ragdollColliders;
+    Rigidbody[] limbsRigidbodies;
+    void GetRagdollBits()
+    {
+        ragdollColliders = rig.GetComponentsInChildren<Collider>();
+        limbsRigidbodies = rig.GetComponentsInChildren<Rigidbody>();
+    }
+
+    void ragdollOn()
+    {
+        foreach (Collider collider in ragdollColliders)
+        {
+            collider.enabled = true;
+        }
+
+        foreach(Rigidbody rigid in limbsRigidbodies)
+        {
+            rigid.isKinematic = false;
+        }
+
+        follow.enabled = false;
+        nav.enabled = false;
+        mainCollider.enabled = false;
+        rb.isKinematic = true;
+    }
+
+    void ragdollOff()
+    {
+        foreach(Collider col in ragdollColliders)
+        {
+            col.enabled = false;
+        }
+
+        foreach(Rigidbody rigid in limbsRigidbodies)
+        {
+            rigid.isKinematic = true;
+        }
+
+        mainCollider.enabled = true;
+        rb.isKinematic = false;
+    }
+
 
     // Update is called once per frame
     void Update()
